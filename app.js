edcc4c02e1866a44b8c2b70e17970076fff215cd
hoangtan15081995 <98004746+hoangtan15081995@users.noreply.github.com>
2022-05-08 08:40:29 +0700

    update

--- app.js
+++ app.js
@@ -1,20 +1,62 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+require("dotenv").config();
+const cors = require("cors");
+const express = require("express");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
+const mongoose = require("mongoose");
+const indexRouter = require("./routes/index");
+const { sendResponse } = require("./helpers/utils");
+const mongoURI = process.env.MONGO_DEV_URI;
+const app = express();
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-
-var app = express();
-
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
+app.use(cors());
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+/* DB Connection */
+mongoose
+  .connect(mongoURI)
+  .then(() => {
+    console.log(`DB connected`);
+    // require("./createFriend.js");
+  })
+  .catch((err) => console.log(err));
+
+app.use("/v1", indexRouter);
+
+// catch 404 and forard to error handler
+app.use((req, res, next) => {
+  const err = new Error("Not Found");
+  err.statusCode = 404;
+  next(err);
+});
+
+/* Initialize Error Handling */
+app.use((err, req, res, next) => {
+  console.log("ERROR", err);
+  if (err.isOperational) {
+    return sendResponse(
+      res,
+      err.statusCode ? err.statusCode : 500,
+      false,
+      null,
+      { message: err.message },
+      err.errorType
+    );
+  } else {
+    return sendResponse(
+      res,
+      err.statusCode ? err.statusCode : 500,
+      false,
+      null,
+      { message: err.message },
+      "Internal Server Error"
+    );
+  }
+});
 
 module.exports = app;
